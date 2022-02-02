ee85a7be20b2129844af50c5e93a124972739e8c
cat_in_136 <cat.in.136+github@gmail.com>
2022-02-03 08:00:55 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::thread::sleep;
+use std::time::Duration;
+use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
+
+fn main() {
+    // Temporary. Will disappear once ESP-IDF 4.4 is released, but for now it is necessary to call this function once,
+    // or else some patches to the runtime implemented by esp-idf-sys might not link properly.
+    esp_idf_sys::link_patches();
+
+    loop {
+        println!("Hello, world!");
+        sleep(Duration::from_secs(1));
+    }
+}
