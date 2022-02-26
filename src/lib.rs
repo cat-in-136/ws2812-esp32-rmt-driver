cc807f80501aaec18d9b413452dfb64e27938744
cat_in_136 <cat.in.136+github@gmail.com>
2022-02-26 11:40:44 +0900

    introduce features: default, unstable, embedded-graphics-core, smart-leds-trait

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,17 @@
+#[cfg(feature = "unstable")]
+pub mod driver;
+#[cfg(not(feature = "unstable"))]
 mod driver;
-mod lib_embedded_graphics;
+
+#[cfg(all(feature = "embedded-graphics-core", feature = "unstable"))]
+pub mod lib_embedded_graphics;
+
+#[cfg(all(feature = "smart-leds-trait", feature = "unstable"))]
+pub mod lib_smart_leds;
+#[cfg(all(feature = "smart-leds-trait", not(feature = "unstable")))]
 mod lib_smart_leds;
 
+#[cfg(feature = "smart-leds-trait")]
 pub use lib_smart_leds::Ws2812Esp32Rmt;
+#[cfg(feature = "smart-leds-trait")]
 pub use smart_leds_trait::RGB8;
