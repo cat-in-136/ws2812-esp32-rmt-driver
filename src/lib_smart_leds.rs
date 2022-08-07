42ce0642e1fdf6d400cccefe3ee4fc01fcb348a0
cat_in_136 <cat.in.136+github@gmail.com>
2022-08-07 15:54:16 +0900

    Introduce generics to lib_smart_leds::Ws2812Esp32Rmt to specify color type

--- src/lib_smart_leds.rs
+++ src/lib_smart_leds.rs
@@ -2,49 +2,82 @@
 
 use crate::driver::color::{LedPixelColor, LedPixelColorGrb24, LedPixelColorImpl};
 use crate::driver::{Ws2812Esp32RmtDriver, Ws2812Esp32RmtDriverError};
-use smart_leds_trait::{SmartLedsWrite, RGB8};
+use smart_leds_trait::{SmartLedsWrite, RGB8, RGBW};
+use std::marker::PhantomData;
+
+/// 8-bit RGBW (RGB + white)
+pub type RGBW8 = RGBW<u8, u8>;
 
 impl<
-    const N: usize,
-    const R_ORDER: usize,
-    const G_ORDER: usize,
-    const B_ORDER: usize,
-    const W_ORDER: usize,
-> From<RGB8> for LedPixelColorImpl<N, R_ORDER, G_ORDER, B_ORDER, W_ORDER>
+        const N: usize,
+        const R_ORDER: usize,
+        const G_ORDER: usize,
+        const B_ORDER: usize,
+        const W_ORDER: usize,
+    > From<RGB8> for LedPixelColorImpl<N, R_ORDER, G_ORDER, B_ORDER, W_ORDER>
 {
     fn from(x: RGB8) -> Self {
         Self::new_with_rgb(x.r, x.g, x.b)
     }
 }
 
-/// ws2812 driver wrapper providing smart-leds API
-pub struct Ws2812Esp32Rmt {
-    driver: Ws2812Esp32RmtDriver,
+impl<
+        const N: usize,
+        const R_ORDER: usize,
+        const G_ORDER: usize,
+        const B_ORDER: usize,
+        const W_ORDER: usize,
+    > From<RGBW8> for LedPixelColorImpl<N, R_ORDER, G_ORDER, B_ORDER, W_ORDER>
+{
+    fn from(x: RGBW8) -> Self {
+        Self::new_with_rgbw(x.r, x.g, x.b, x.a.0)
+    }
 }
 
-impl Ws2812Esp32Rmt {
+/// ws2812 driver wrapper providing smart-leds API
+pub struct Ws2812Esp32Rmt<CSmart = RGB8, CDev = LedPixelColorGrb24>
+where
+    CDev: LedPixelColor + From<CSmart>,
+{
+    driver: Ws2812Esp32RmtDriver,
+    phantom: PhantomData<(CDev, CSmart)>,
+}
+
+impl<CSmart, CDev> Ws2812Esp32Rmt<CSmart, CDev>
+where
+    CDev: LedPixelColor + From<CSmart>,
+{
     /// Create a new driver wrapper.
     ///
     /// `channel_num` shall be different between different `gpio_num`.
     pub fn new(channel_num: u8, gpio_num: u32) -> Result<Self, Ws2812Esp32RmtDriverError> {
         let driver = Ws2812Esp32RmtDriver::new(channel_num, gpio_num)?;
-        Ok(Self { driver })
+        Ok(Self {
+            driver,
+            phantom: Default::default(),
+        })
     }
 }
 
-impl SmartLedsWrite for Ws2812Esp32Rmt {
+impl<CSmart, CDev> SmartLedsWrite for Ws2812Esp32Rmt<CSmart, CDev>
+where
+    CDev: LedPixelColor + From<CSmart>,
+{
     type Error = Ws2812Esp32RmtDriverError;
-    type Color = RGB8;
+    type Color = CSmart;
 
     fn write<T, I>(&mut self, iterator: T) -> Result<(), Self::Error>
     where
         T: Iterator<Item = I>,
         I: Into<Self::Color>,
     {
-        let pixel_data: Vec<u8> = iterator
-            .flat_map(|color| LedPixelColorGrb24::from(color.into()).0)
-            .collect();
-        self.driver.write(&pixel_data)
+        let mut pixel_data = Vec::new();
+        for color in iterator {
+            for v in CDev::from(color.into()).as_ref() {
+                pixel_data.push(*v)
+            }
+        }
+        self.driver.write(pixel_data.as_slice())
     }
 }
 
