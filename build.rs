8242da4ed7895252a81533fa8b9138000af72b72
Linus Färnstrand <faern@faern.net>
2024-01-07 15:13:42 +0100

    Use standard Result type instead of anyhow since it's not needed

--- build.rs
+++ build.rs
@@ -1,9 +1,8 @@
 // Necessary because of this issue: https://github.com/rust-lang/cargo/issues/9641
-fn main() -> anyhow::Result<()> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     if std::env::var("CARGO_CFG_TARGET_VENDOR") == Ok("espressif".to_string()) {
         embuild::build::CfgArgs::output_propagated("ESP_IDF")?;
-        embuild::build::LinkArgs::output_propagated("ESP_IDF")
-    } else {
-        Ok(())
+        embuild::build::LinkArgs::output_propagated("ESP_IDF")?
     }
+    Ok(())
 }
