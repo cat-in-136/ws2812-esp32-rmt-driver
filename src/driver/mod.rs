47f9a3613b5eb33303a0fd6fb4c97b3fe5f65975
cat_in_136 <cat.in.136+github@gmail.com>
2024-03-13 20:42:44 +0900

    simplify src/driver/mod.rs

--- src/driver/mod.rs
+++ src/driver/mod.rs
@@ -1,13 +1,9 @@
 //! Low-level LED pixel driver API.
 
 pub mod color;
-#[cfg(target_vendor = "espressif")]
-mod esp32_rmt;
-#[cfg(not(target_vendor = "espressif"))]
-mod mock;
 
-#[cfg(not(target_vendor = "espressif"))]
-use mock as esp32_rmt;
+#[cfg_attr(not(target_vendor = "espressif"), path = "mock.rs")]
+mod esp32_rmt;
 
 pub use esp32_rmt::Ws2812Esp32RmtDriver;
 pub use esp32_rmt::Ws2812Esp32RmtDriverError;
